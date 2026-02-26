7cdbe11ec6f30572e08273c94612ad466d15b6d2
Matteo CIpollina <matteo.cipollina@yahoo.it>
2026-02-26 07:12:44 +0100

    Update OSforGFF.lean

--- OSforGFF.lean
+++ OSforGFF.lean
@@ -76,10 +76,16 @@ import «OSforGFF».NuclearSpace.Std
 import «OSforGFF».NuclearSpace.Transport
 import «OSforGFF».NuclearSpace.RapidDecaySeq
 import «OSforGFF».NuclearSpace.RapidDecaySeqBase
+import «OSforGFF».NuclearSpace.RapidDecaySeqIndex
 import «OSforGFF».NuclearSpace.RapidDecaySeqMulti
+import «OSforGFF».NuclearSpace.RapidDecaySeqMultiIndex
 import «OSforGFF».NuclearSpace.HermiteSchwartz
 import «OSforGFF».NuclearSpace.PhysHermite
 import «OSforGFF».NuclearSpace.PhysHermiteGaussL2Basis
+import «OSforGFF».NuclearSpace.PhysHermiteMultiCoeffNuclearity
+import «OSforGFF».NuclearSpace.PhysHermiteMultiCoeffSeminorm
+import «OSforGFF».NuclearSpace.PhysHermiteMulti
+import «OSforGFF».NuclearSpace.PhysHermiteMultiLadder
 import «OSforGFF».NuclearSpace.PhysHermiteSchwartz
 import «OSforGFF».NuclearSpace.PhysHermiteSchwartzLadder
 import «OSforGFF».NuclearSpace.PhysHermiteL2Basis
